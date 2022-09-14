526305312bbdc0d9e01e8b234e2236a36b8b8ba3
Bahadır <m.bahadirtasli@gmail.com>
2022-09-14 20:05:27 +0300

    Helloworld changed to thefirstprogram

---
+++ src/Datatype.java
@@ -0,0 +1,30 @@
+/**
+ * Examples of primitive datatype
+ *
+ * @author bahadirtasli
+ */
+public class Datatype {
+    int a = 1;
+    byte byteNumber = 120;
+    long longNumber = 1234554321L;
+    short shortNumber = -12321;
+    float decimalNumber = 1.23f;
+    double doubleDecimalnumber = 1.123123123123123;
+
+    //Character
+    char alpha = "F";
+
+    //True/False
+    boolean hadLunch = true ;// had lunch?
+    boolean isHoliday = false ;
+
+    /* Multi-line
+    comment
+     */
+
+    int i; // 0
+    float x; // 0.00f
+    char animal;// ""
+
+    boolean isClassTomorrow; // false
+}
