48e85ea3b99aefbc44997157a26c45ad4d944642
Bahadır <m.bahadirtasli@gmail.com>
2022-09-14 20:53:57 +0300

    Helloworld changed to thefirstprogram

--- src/Thefirstprogram.java
+++ src/Thefirstprogram.java
@@ -1,11 +1,13 @@
 /**
  *
- * @author Bahadir Tasli
+ * @author bahadirtasli
  */
 
 public class Thefirstprogram {
 
     public static void main(String[] args) {
         System.out.println("Hello,World!");
+
+        System.out.println(Datatype.FOOD);
     }
 }
