ab200152c6be8faf14165838fbee7eb555dd9c68
Bahadır <m.bahadirtasli@gmail.com>
2022-09-20 18:12:26 +0300

    Homework2

---
+++ src/Homework2.java
@@ -0,0 +1,29 @@
+/**
+ * Statement examples
+ *
+ * @author bahadir tasli
+ */
+public class Homework2 {
+    public static void main(String[] args) {
+
+        int a = 15, b =25;
+
+        int c;
+
+        c = a;
+        a = b;
+        b = c;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        //if ((bucket1 > bucket2 && bucket3 > 0) && bucket1 ==15) {
+          //  System.out.println("case 1:bucket 1= 15 & bucket 2 = null & bucket 3 = 25");
+        //} else if ((bucket3 > bucket1) && bucket1 > 0) {
+          //  System.out.println("case 2:bucket 1= 0 & bucket 2 = 15 & bucket 3 = 25");
+        //}
+
+
+
+    }
+}
