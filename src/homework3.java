954b89660dcdabf3ecc0267734fa722965608f98
Bahadır <m.bahadirtasli@gmail.com>
2022-09-21 22:34:16 +0300

    Homework3

---
+++ src/homework3.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * Examples
+ *
+ * @author bahadir tasli
+ */
+public class homework3 {
+    public static void main(String[] args) {
+        int j = 10;
+        System.out.println(((j % 3 == 0) && ((j % 5) == 0)) ? "FIZZBUZZ" : (j % 3 == 0) ? "FIZZ" : (j % 5 == 0) ? "BUZZ" : "");
+    }
+}
