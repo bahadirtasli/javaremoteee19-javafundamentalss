a8676aea8e72101ca0ca457f210af366ef7f29e1
Bahadır <m.bahadirtasli@gmail.com>
2022-09-22 12:28:51 +0300

    Homework2

--- src/Homework2.java
+++ src/Homework2.java
@@ -8,22 +8,12 @@ public class Homework2 {
 
         int a = 15, b =25;
 
-        int c;
-
-        c = a;
+        int temp = a;
         a = b;
-        b = c;
+        b = temp;
 
-        System.out.println(a);
-        System.out.println(b);
-
-        //if ((bucket1 > bucket2 && bucket3 > 0) && bucket1 ==15) {
-          //  System.out.println("case 1:bucket 1= 15 & bucket 2 = null & bucket 3 = 25");
-        //} else if ((bucket3 > bucket1) && bucket1 > 0) {
-          //  System.out.println("case 2:bucket 1= 0 & bucket 2 = 15 & bucket 3 = 25");
-        //}
-
-
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
 
     }
 }
