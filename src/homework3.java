5855314cbe9f85ba5f028fb94a1194511defdbc9
Bahadır <m.bahadirtasli@gmail.com>
2022-09-22 12:27:12 +0300

    Homework3

--- src/homework3.java
+++ src/homework3.java
@@ -7,6 +7,6 @@
 public class homework3 {
     public static void main(String[] args) {
         int j = 10;
-        System.out.println(((j % 3 == 0) && ((j % 5) == 0)) ? "FIZZBUZZ" : (j % 3 == 0) ? "FIZZ" : (j % 5 == 0) ? "BUZZ" : "");
+        System.out.println(((j % 3 == 0) && ((j % 5) == 0)) ? "FIZZBUZZ" : (j % 3 == 0) ? "FIZZ" : (j % 5 == 0) ? "BUZZ" : "") ;
     }
 }
