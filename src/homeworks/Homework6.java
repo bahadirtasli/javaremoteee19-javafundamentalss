50950bdf1aabf626ec97df607598745f8fa33eaf
Bahadır <m.bahadirtasli@gmail.com>
2022-09-27 14:48:22 +0300

    Homework6

---
+++ src/homeworks/Homework6.java
@@ -0,0 +1,22 @@
+package homeworks;
+
+/**
+ *
+ * Homework6
+ *
+ * @author bahadir tasli
+ */
+
+public class Homework6 {
+    public static void main(String[] args) {
+        int[] i = {1, 7, 3, 10, 9};
+        int largestelement = i[0];
+
+        for (int x : i) {
+            if (x > largestelement) {
+                largestelement = x;
+            }
+        }
+        System.out.println(largestelement);
+    }
+}
